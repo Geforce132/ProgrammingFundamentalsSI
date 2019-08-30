464d73a2a9ad2e30adb89616c53810c8de3ad1ba
Geforce <Geforce132@gmail.com>
2019-08-30 02:33:30 -0500

    Added activities for 8/30 session

---
+++ Activities/August 30 10-30AM/P2.cpp
@@ -0,0 +1,31 @@
+// This program will calculate a student's GPA based on their
+// grades so far. This particular student has attempted 18 credit 
+// hours and has gotten 2 As, 2 Bs, a C, and an F.
+// Letter grades translate into the following number of points:
+// A = 4.0
+// B = 3.0
+// C = 2.0
+// D = 1.0
+// F = 0.0
+// GPAs are calculated by dividing the number of points earned by
+// the number of credit hours attempted
+
+/**********/
+/**********/
+
+int main() 
+{
+	/******/ creditHours = 18;
+	/******/
+	/******/
+	/******/
+	/******/
+	/******/
+	/******/
+
+	double gpa = /******/ / creditHours;
+
+	cout << "Your GPA is " << (/******/ * 4);
+
+	return 0;
+}
---
+++ Activities/August 30 10-30AM/P3.cpp
@@ -0,0 +1,23 @@
+// This program will calculate the weekly paycheck of an employee
+// who works 19.5 hours a week at $12.75 an hour with an 8.25%
+// income tax
+
+/****************/
+/****************/
+
+int main() 
+{
+	/**********************/
+	/**********************/
+	double incomeTaxRate = /********/;
+
+	double income = /***********/;
+	double tax = income * incomeTaxRate;
+
+	income = /********/ tax;
+
+	cout << "Wages before tax is $" << /**********/ << ".\n\n";
+	cout << "Wages after tax is $" << /***********/ << ".\n\n";
+
+	/***********/
+}
