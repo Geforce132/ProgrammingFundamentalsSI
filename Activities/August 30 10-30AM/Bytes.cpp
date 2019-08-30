59df17f03f000dc00d2c53d29ed334df0b4c87a3
Geforce <Geforce132@gmail.com>
2019-08-29 23:33:17 -0500

    Added closer for 8/30 session

---
+++ Activities/August 30 10-30AM/Bytes.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int bytes = sizeof(*insert variable type here*);
+	cout << "The size of this variable type is " << bytes << " bytes.";
+
+	return 0;
+}
