c31cb369e42fdd93e3eedccff5bcffd44ef4968b
Geforce <Geforce132@gmail.com>
2019-09-04 02:21:29 -0500

    Clarified and removed some comments from P3

--- Activities/September 4 9-30AM/P3.cpp
+++ Activities/September 4 9-30AM/P3.cpp
@@ -2,22 +2,22 @@
 // who works 19.5 hours a week at $12.75 an hour with an 8.25%
 // income tax
 
-/****************/
-/****************/
+/********/
+/********/
 
-int main() 
+int main()
 {
-	/**********************/
-	/**********************/
+	/********/
+	/********/
 	double incomeTaxRate = /********/;
 
-	double income = /***********/;
-	double tax = income * incomeTaxRate;
+	double income = /********/;
+	double tax = /********/;
 
-	income = /********/ tax;
+	/********/
 
-	cout << "Wages before tax is $" << /**********/ << ".\n\n";
-	cout << "Wages after tax is $" << /***********/ << ".\n\n";
+	cout << "Wages before tax is $" << /********/ << ".\n\n";
+	cout << "Wages after tax is $" << /********/ << ".\n";
 
-	/***********/
+	return 0;
 }
