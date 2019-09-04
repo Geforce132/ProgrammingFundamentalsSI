445d354a33f41527b78e5436b2d7f1002c82afdf
Geforce <Geforce132@gmail.com>
2019-09-04 02:13:22 -0500

    Fixed some math formula mistakes for P2

--- Activities/September 4 9-30AM/P2.cpp
+++ Activities/September 4 9-30AM/P2.cpp
@@ -1,31 +1,34 @@
 // This program will calculate a student's GPA based on their
 // grades so far. This particular student has attempted 18 credit 
-// hours and has gotten 2 As, 2 Bs, a C, and an F.
+// hours (3 credits per class) and has gotten 2 As, 2 Bs, a C, and an F.
 // Letter grades translate into the following number of points:
-// A = 4.0
-// B = 3.0
-// C = 2.0
-// D = 1.0
-// F = 0.0
-// GPAs are calculated by dividing the number of points earned by
+//     A = 4.0
+//     B = 3.0
+//     C = 2.0
+//     D = 1.0
+//     F = 0.0
+//
+// Formula for calculating the number of points earned per grade:
+//    (letter grade points) * number of credit hours attempted
+//
+// GPAs are calculated by dividing the total number of points earned by
 // the number of credit hours attempted
 
-/**********/
-/**********/
+/***************/
+/***************/
 
-int main() 
+int main()
 {
-	/******/ creditHours = 18;
-	/******/
-	/******/
-	/******/
-	/******/
-	/******/
-	/******/
+	int creditHours = 18;
+	double aGradePoints = /***************/ * 6;
+	/***************/
+	/***************/
+	/***************/
+	/***************/
 
-	double gpa = /******/ / creditHours;
+	double gpa = (/***************/) / /***************/;
 
-	cout << "Your GPA is " << (/******/ * 4);
+	cout << "Your GPA is " << /***************/;
 
 	return 0;
 }
