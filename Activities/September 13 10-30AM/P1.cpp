44f458666234bd0de7b64627faabeac19b5ccb65
Geforce <Geforce132@gmail.com>
2019-09-13 02:08:15 -0500

    Updated P1 | updated session folder name

---
+++ Activities/September 13 10-30AM/P1.cpp
@@ -0,0 +1,37 @@
+// This program will ask the user to enter the number of
+// people that are attending a party, as well as the number
+// of pizza slices that are available for everyone to share.
+// It will calculate an even amount of pizza that can be shared among
+// everyone and display that number onto the screen. The value for the
+// amount of pizza slices that everyone will have can only have one digit
+// after the decimal point.
+
+/*****************/
+/*****************/
+using namespace std;
+
+int main()
+{
+	int /*****************/;
+	int /*****************/;
+	double /*****************/;
+
+	cout << "How many people are attending the party? " << endl;
+	cin >> /*****************/;
+
+	cout << "How many slices of pizza are there? " << endl;
+	cin >> /*****************/;
+
+	/*****************/ = /*****************/ / numOfPeople;
+
+	cout << "Each of the " << numOfPeople << " people can have " << /*****************/ << " slices of pizza.\n\n";
+
+	cout << "Oh no! One person left the party!\n";
+
+	numOfPeople /*****************/;
+	/*****************/ = /*****************/ / numOfPeople;
+
+	cout << "Now, the " << numOfPeople << " remaining people can have " << /*****************/ << " slices of pizza each.\n";
+
+	return 0;
+}
